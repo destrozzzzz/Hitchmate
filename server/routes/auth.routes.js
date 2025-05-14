312fcb88af6d9b232d398b074116eeb7a252e282
Khamar ali <125788672+destrozzzzz@users.noreply.github.com>
2025-05-14 23:37:53 +0530

    Update auth.routes.js
    the verify token removed from the auth.routes.js , which was giving problem logging out after the token was expired

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { login, logout, register } from "../controllers/auth.js";
-import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router()
 
 router.post("/register", register)
 router.post("/login", login)
-router.post("/logout", verifyToken, logout)
+router.post("/logout", logout)
 
 export default router
